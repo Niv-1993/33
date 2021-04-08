b18d34a3f26472e9c69ffbbb1fa0f48ce71e46db
BARDAMRI <batdam@post.bgu.ac.il>
2021-04-08 11:36:14 +0300

    adding explanations

--- dev/src/Menu.java
+++ dev/src/Menu.java
@@ -26,12 +26,17 @@ public class Menu {
     }
 
     /**
-     *
+     *The starting choise of the user if to keep run the system or shut it off.
      */
     public void chooseOption(){
         System.out.println("press 1 to see all Transportations, press 2 to create a new Transportation");
         option = chooseOp(numOfOptions);
     }
+
+    /**
+     * prints menu and received the user's choise for which area is the new transportation.
+     * @param t : the presentation's transportation object to show the user and to contact the business layer.
+     */
     private void chooseArea(TransportationServiceDTO t){
         System.out.println("Please chose an Area");
         for (int i=0; i<areas.length;i++) {
@@ -43,6 +48,11 @@ public class Menu {
         t.setArea(chosen);
         controller.setTransportationArea(t);
     }
+
+    /**
+     *Asks and received the user's input for the wanted trans hour.
+     * @param tran : the presentation's transportation object to show the user and to contact the business layer.
+     */
     private void chooseTime(TransportationServiceDTO tran){
         boolean success=false;
         while (!success) {
@@ -59,6 +69,11 @@ public class Menu {
             }
         }
     }
+
+    /**
+     * Asks and received the user's input for the wanted trans date.
+     * @param tran: the presentation's transportation object to show the user and to contact the business layer.
+     */
     private void chooseDate(TransportationServiceDTO tran){
         boolean success=false;
         while (!success) {
@@ -75,6 +90,10 @@ public class Menu {
             }
         }
     }
+
+    /**
+     * This is the add transportation menu method. the whole user's input and dialog runs/called from here.
+     */
     private void chooseAddOption(){
         finish=false;
         TransportationServiceDTO newTrans=controller.createNewTransportation();
@@ -100,6 +119,11 @@ public class Menu {
             System.out.println(newTrans);
         }
     }
+
+    /**
+     *Asks and received the user's input for the wanted trans weight.
+     * @param t: the presentation's transportation object to show the user and to contact the business layer.
+     */
     private void chooseWeight(TransportationServiceDTO t){
         try {
             System.out.println("please enter transportation total weight:");
@@ -111,6 +135,13 @@ public class Menu {
             t.setTruck(null);
         }
     }
+
+    /**
+     *Method that called by the addTransportation menu. try to close the new trans with the all new data.
+     * If one field is empty the method will no allow.
+     * If finished, back to first menu.
+     * @param t: the presentation's transportation object to show the user and to contact the business layer
+     */
     private void submit(TransportationServiceDTO t){
         try {
 
@@ -121,6 +152,13 @@ public class Menu {
             System.out.println(e.getMessage());
         }
     }
+
+    /**
+     *Menu of suppliers and their items.
+     * The method ask for a flow of suppliers until -1 number.
+     * After each input, ask in a loop for item and quantity until -2 number.
+     * @param t: the presentation's transportation object to show the user and to contact the business layer
+     */
     private void chooseSupplier(TransportationServiceDTO t) {
         try {
             System.out.println("please select suppliers and items from the lists below, press -1 to finish:");
@@ -158,6 +196,13 @@ public class Menu {
             System.out.println("Error: "+e.getMessage());
         }
     }
+
+    /**
+     *Menu of suppliers and their items.
+     * The method ask for a flow of brances until -1 number.
+     * After each input, ask in a loop for item and quantity until -2 number.
+     * @param t: the presentation's transportation object to show the user and to contact the business layer
+     */
     private void chooseBranch(TransportationServiceDTO t) {
         try {
             System.out.println("please select branches and items from the lists below, press -1 to finish:");
@@ -195,6 +240,12 @@ public class Menu {
             System.out.println("Error: "+e.getMessage());
         }
     }
+
+    /**
+     *Asks and received from user the driver for the transportation by id number.
+     * If the addition could not complete, the transportation's driver's details will not be changed.
+     * @param t: the presentation's transportation object to show the user and to contact the business layer
+     */
     private void chooseDriver(TransportationServiceDTO t) {
         try {
             System.out.println("please select driver id from the trucks list below:");
@@ -208,6 +259,11 @@ public class Menu {
         }
     }
 
+    /**
+     *Asks and received from user the truck for the transportation by id number.
+     * If the addition could not complete, the transportation's truck's details will not be changed.
+     * @param t: the presentation's transportation object to show the user and to contact the business layer
+     */
     private void chooseTruck(TransportationServiceDTO t) {
 
         try {
@@ -222,38 +278,74 @@ public class Menu {
         }
     }
 
+    /**
+     *Prints to the user all the available drivers in the database
+     */
     public void printAllDrivers(){
        List<DriverServiceDTO> lis=controller.getAllDrivers();
         for (DriverServiceDTO dri:lis) { System.out.println(dri); }
     }
+
+    /**
+     *Prints to the user all the available trucks in the database
+     */
     public void printAllTucks(){
         List<TruckServiceDTO> lis=controller.getAllTrucks();
         for (TruckServiceDTO tru:lis) { System.out.println(tru); }
     }
+
+    /**
+     *Prints to the user all the available branches in the database
+     */
     public void printAllBranches(){
         List<BranchServiceDTO> bra=controller.getAllBranches();
         for (BranchServiceDTO tru:bra) { System.out.println(tru); }
     }
+
+    /**
+     *Prints to the user all the available suppliers in the database
+     */
     public void printAllSuppliers(){
         List<SupplierServiceDTO> sup=controller.getAllSuppliers();
         for (SupplierServiceDTO tru:sup) { System.out.println(tru); }
 
     }
+
+    /**
+     *Prints to the user all the available items in the database
+     */
     public void PrintAllItems(){
         List<ItemServiceDTO> sup=controller.getAllItems();
         for (ItemServiceDTO tru:sup) { System.out.println(tru); }
 
     }
+
+    /**
+     *Prints to the user all the available transportations in the database
+     * Used for the transportations printing option in the menu.
+     */
     public void printAllTransportations(){
         List<TransportationServiceDTO> sup=controller.getAllTransportations();
         for (TransportationServiceDTO tru:sup) { System.out.println(tru); }
     }
 
+    /**
+     *The starting menu of the system.
+     * runs by the main of the project.
+     * By user's input it keep running the system or shut it off.
+     * @return
+     */
     public boolean endOfProgram(){
         System.out.println("to continue press 1, to end press 2");
         int numOfEndProgramOp = 2;
         return chooseOp(numOfEndProgramOp) == 1;
     }
+
+    /**
+     *Method to receive an input from the user with boundary limit.
+     * @param con : the num of options the user can type. For boundary check.
+     * @return : the choise of the user.
+     */
     private int chooseOp(int con){
         boolean validInput = false;
         int userOption = -1;
@@ -268,6 +360,9 @@ public class Menu {
         return userOption;
     }
 
+    /**
+     *Method to direct the menu by the user's choise in the starting menu.
+     */
     public void nextStep() {
 
         if(option==2)
