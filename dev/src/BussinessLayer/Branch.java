fc6bef7e9c0045780f54495a201db3551fc7a58f
BARDAMRI <batdam@post.bgu.ac.il>
2021-04-09 18:18:55 +0300

    edit classes by diagram

--- dev/src/BussinessLayer/Branch.java
+++ dev/src/BussinessLayer/Branch.java
@@ -1,5 +1,5 @@
 package BussinessLayer;
-//yuval
+
 public class Branch extends Site{
     public Branch(String phone, String contactName, int id, Address address, ShippingArea shippingArea) {
         super(phone, contactName, id, address, shippingArea);
