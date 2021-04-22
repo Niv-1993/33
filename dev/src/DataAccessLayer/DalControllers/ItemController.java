901a78bd144225e79e54d3fdc1e0c7ecd015b3b1
kfirs127 <62557231+kfirs127@users.noreply.github.com>
2021-04-22 11:58:38 +0300

    added methodes to controller

--- dev/src/DataAccessLayer/DalControllers/ItemController.java
+++ dev/src/DataAccessLayer/DalControllers/ItemController.java
@@ -2,11 +2,43 @@ package DataAccessLayer.DalControllers;
 
 import DataAccessLayer.DalObjects.DalItem;
 
+import java.util.List;
+
 public class ItemController extends DalController<DalItem> {
 
     private final String tableName = "Items";
 
     public ItemController() {
         super("Items");
+    }
+
+    @Override
+    public boolean insert(DalItem dalObject) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(DalItem dalObject) {
+        return false;
+    }
+
+    @Override
+    public DalItem reader(DalItem dalObject) {
+        return null;
+    }
+
+    @Override
+    public List<DalItem> loadData(DalItem dalObject) {
+        return null;
+    }
+
+    @Override
+    public boolean update(DalItem dalObject) {
+        return false;
+    }
+
+    @Override
+    public List<DalItem> select(DalItem dalObject) {
+        return null;
     }
 }
--- dev/src/DataAccessLayer/DalControllers/SupplierCardController.java
+++ dev/src/DataAccessLayer/DalControllers/SupplierCardController.java
@@ -2,9 +2,41 @@ package DataAccessLayer.DalControllers;
 
 import DataAccessLayer.DalObjects.DalSupplierCard;
 
+import java.util.List;
+
 public class SupplierCardController extends DalController<DalSupplierCard>{
 
     private final String tableName = "SupplierCards";
 
     public SupplierCardController(){super("SupplierCards");}
+
+    @Override
+    public boolean insert(DalSupplierCard dalObject) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(DalSupplierCard dalObject) {
+        return false;
+    }
+
+    @Override
+    public DalSupplierCard reader(DalSupplierCard dalObject) {
+        return null;
+    }
+
+    @Override
+    public List<DalSupplierCard> loadData(DalSupplierCard dalObject) {
+        return null;
+    }
+
+    @Override
+    public boolean update(DalSupplierCard dalObject) {
+        return false;
+    }
+
+    @Override
+    public List<DalSupplierCard> select(DalSupplierCard dalObject) {
+        return null;
+    }
 }
