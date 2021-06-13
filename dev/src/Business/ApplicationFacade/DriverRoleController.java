f70919a264603ed57e355d6ca9fd2d019caaebcc
Niv <nivdan7@gmial.com>
2021-06-13 14:59:27 +0300

    AA

--- dev/src/Business/ApplicationFacade/DriverRoleController.java
+++ dev/src/Business/ApplicationFacade/DriverRoleController.java
@@ -47,7 +47,22 @@ public class DriverRoleController implements iDriverRoleController {
         String shiftType = leavingTime.getHour() < 14 ? "Morning" : "Night";
         mc.removeDriverFromShiftAndStorekeeper(driverID,date,shiftType);
     }
+    // INSERT INTO ManagerAlerts (BID, EID, Date, Message) VALUES(?,?,?,?);
+    public List<Integer> allPersonnelManager(int BID){
+        return null;
+    }
+    //1
+    public List<Integer> checkAvailableDriverSubs(int driverID, LocalTime time, LocalDate date,List<Integer> optionalDrivers){
+        return null;
+    }
+    //2
+    public void swapDrivers(int newDriverID, int oldDriverID, LocalTime time, LocalDate date){
+        //CHANGEDRIVER
+    }
 
+    public void changeDriver(List<Integer> BIDS, int oldDriverID, int newDriverID,  LocalDate date, LocalTime leavingTime){
+
+    }
     public List<Driver> chooseDriver(LocalDate date, LocalTime leavingTime) throws Exception {
         String shiftType = leavingTime.getHour() < 14 ? "Morning" : "Night";
         List<Integer> driversID =  mc.getAllAvailableDrivers(date,shiftType);
