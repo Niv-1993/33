f938d29bb934d0a535b02e2408479673cc3ba099
orishac <62713593+orishac@users.noreply.github.com>
2021-06-14 16:35:31 +0300

    added transactions to add objects to DB

--- dev/src/Presentation/Controllers.java
+++ dev/src/Presentation/Controllers.java
@@ -7,14 +7,12 @@ import Business.ApplicationFacade.iControllers.iRegularRoleController;
 import Business.StockBusiness.StoreController;
 import Business.StockBusiness.iStoreController;
 import Business.SupplierBusiness.ISupplierService;
+import Business.SupplierBusiness.Item;
 import Business.SupplierBusiness.facade.SupplierService;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Controllers {
     private final iRegularRoleController rc;
@@ -147,10 +145,99 @@ public class Controllers {
         mc.createWeekShifts();
         rc.Logout();
 
+        ////
         sc.addSupplier("Supplier1", 0,0,0,"cash");
         sc.addSupplier("Supplier2", 1,1,1,"bank transfer");
-        sc.addSupplier("Supplier3", 2,2,2,"");
-        sc.addSupplier("Supplier4", 3,3,3,"cash");
+        sc.addSupplier("Supplier3", 2,2,2,"bank transfer");
+        sc.addSupplier("Supplier4", 3,3,3,"check");
+        sc.addSupplier("Supplier5", 4,4,4,"cash");
+        sc.addItem(0 ,0 ,"meat" , 21.99 , 19.99, 10,
+                "Tnuva", 1, LocalDate.now().plusYears(1), 1).getOutObject();
+        sc.addItem(0 ,0 ,"minced meat" ,  39.99, 34.99, 100,
+                "Dabah", 1, LocalDate.now().plusYears(1), 2);
+        sc.addItem(0 ,1, "stake" , 70.5, 65, 50,
+                "Dabah", 1,  LocalDate.now().plusMonths(1), 1.5);
+        sc.addItem(1 ,2, "yogurt" ,  9.99, 8.99, 200,
+                "Tara", 2, LocalDate.now().plusMonths(1), 0.1);
+        sc.addItem(1 ,3, "cheese cake",  35, 34, 150,
+                "Cheese Cake Factory",2, LocalDate.now().plusDays(14), 1);
+        sc.addItem(1 ,0, "gum"  ,1.99, 1.5, 1000,
+                "Hubba", 3, LocalDate.now().plusYears(1000), 0.01);
+        sc.addItem(1 , 1,  "lollipop" ,  0.5, 0.35, 1000,
+                "Hubba", 3, LocalDate.now().plusYears(1000), 0.01);
+        sc.addItem(2 , 2, "windshield wiper",  15.99, 14, 500,
+                "Sano", 3, LocalDate.now().plusYears(1), 0.5);
+        sc.addItem(2 , 3,  "broom", 19.99, 17, 400,
+                "Sano", 3, LocalDate.now().plusYears(50), 1);
+        sc.addItem(3 , 0,"soda" , 10.99 , 9, 350,
+                "Cola-Cola", 3, LocalDate.now().plusMonths(6), 3);
+        sc.addItem(3 , 0,"sprite ",  10.99 , 8, 700,
+                "Cola-Cola", 3, LocalDate.now().plusMonths(3), 1.5);
+        sc.addItem(3 ,1, "fanta" ,10.99, 10, 200,
+                "Cola-Cola", 3, LocalDate.now().plusMonths(3), 1.5);
+        sc.addItem(3 , 1,"grape juice" , 10.99 , 9.99, 350,
+                "PriGat", 3, LocalDate.now().plusMonths(9), 1);
+        sc.addItem(3 , 2,"orange juice" ,  10.99 , 9.99, 400,
+                "PriGat", 3, LocalDate.now().plusMonths(9), 1);
+        sc.addItem(3 , 2, "water", 5.99 , 4.5, 650,
+                "Neviot", 3, LocalDate.now().plusYears(3), 2);
+        sc.addItem(4 , 3,  "apple", 1.99 , 1.5, 1200,
+                "Sami", 3, LocalDate.now().plusMonths(1), 0.05);
+        sc.addItem(4 , 3,  "orange",1.99 , 1.5, 1300,
+                "Sami", 3, LocalDate.now().plusMonths(1), 0.05);
+        sc.addItem(4 , 0,"tomato" ,0.99 , 0.5, 1150,
+                "Orna", 3, LocalDate.now().plusMonths(1), 0.05);
+        sc.addItem(4 , 0, "onion" ,1.5 , 1, 1200,
+                "Orna", 3, LocalDate.now().plusMonths(1), 0.05);
+        for(int i = 0 ; i < 19 ; i++  ){
+            if(i<= 2) sc.addQuantityDocument(0 , i , 3 + i , 2 + i);
+            else if(i<= 6) sc.addQuantityDocument(1 , i , 2 + i , 1 + i);
+            else if(i<= 8) sc.addQuantityDocument(2 , i , i , 3);
+            else if(i<= 14) sc.addQuantityDocument(3 , i , i - 8 ,1);
+            else sc.addQuantityDocument(4 , i , i - 14 , i-14);
+        }
+        sc.addSupplierAgreement(0 , 200 , 1 , true , true);
+        sc.addSupplierAgreement(1 , 100 , 0 , false , true);
+        sc.addSupplierAgreement(2 , 150 , 2 , true , false);
+        sc.addSupplierAgreement(3 , 100 , 1 , false , false);
+        sc.addContactEmail(0 , "BGU@bgu.ac.il" , "Rami");
+        sc.addContactEmail(0 , "Lucture@bgu.ac.il" , "Rami");
+        sc.addContactEmail(1 , "Intel@gamil.com" , "Lidor");
+        sc.addContactEmail(3 , "PleaseGiveUs100@bgu.ac.il" , "Kfir");
+        sc.addContactEmail(3 , "PleaseGiveUs100@gamil.com" , "Ori");
+        sc.addContactEmail(4 , "AmirTheKing@bgu.ac.il" , "Amir");
+        sc.addContactEmail(4 , "Cotel@GOD.com" , "Gabriel");
+        sc.addContactEmail(4 , "SneBoher@GOD.com" , "Moshes");
+        sc.addContactPhone(0 , "050-0000000" , "Tali");
+        sc.addContactPhone(1 , "000-0000000" , "Jesus");
+        sc.addContactPhone(2 , "054-1234567" , "Hani");
+        sc.addContactPhone(3 , "054-9849521" , "Kfir");
+        sc.addContactPhone(3 , "052-4737283" , "Ori");
+        Hashtable<Integer, Integer> firstHash = new Hashtable<>();
+        Hashtable<Integer, Integer> secondHash = new Hashtable<>();
+        Hashtable<Integer, Integer> thirdHash = new Hashtable<>();
+        firstHash.put(1, 100);
+        firstHash.put(2, 50);
+        firstHash.put(3,25);
+        secondHash.put(4,45);
+        secondHash.put(5,5);
+        secondHash.put(6,10);
+        thirdHash.put(7,20);
+        thirdHash.put(8,50);
+        thirdHash.put(9,150);
+        thirdHash.put(10,5);
+        sc.addRegularOrder(0, 1, firstHash);
+        sc.addRegularOrder(1,2, secondHash);
+        sc.addRegularOrder(2,2,thirdHash);
+        sc.addNeededOrder(0, 3,1);
+        sc.addNeededOrder(1, 1 ,2);
+        sc.addNeededOrder(2,2,3);
+        sc.addItemToOrder(0 , 0 , 0 , 2);
+        sc.addItemToOrder(0 , 0 , 2 , 5);
+        sc.addItemToOrder(2 , 1 , 8 , 10);
+        sc.addItemToOrder(3 , 2 ,  11, 4);
+        sc.addItemToOrder(4 , 3 , 15 , 5);
+        sc.addItemToOrder(4 , 4 , 18 , 1);
     }
 
     public int getCurrBID() {
